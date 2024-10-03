0df4366309bc376800fc830fb157bbc4f2a4d44c
Edward Potter <74555065+TA-Solaris@users.noreply.github.com>
2024-10-03 19:14:19 +0100

    Added Gallery component

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className="App">
-      
+      <Gallery />
     </div>
   );
 }
---
+++ react/src/components/Gallery.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Gallery extends React.Component {
+
+    // TODO - Load images from backend
+    // TODO - Add image caching
+
+    render() {
+        const galleryStyle = {
+            padding: "10px",
+        };
+
+        // TODO - Add images
+        return (
+            <section style={galleryStyle}>
+
+            </section>
+        );
+    }
+
+}
+
+export default Gallery;
