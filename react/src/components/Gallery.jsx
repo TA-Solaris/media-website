d9edce16fdca301a76fe2ed4f13f8b626dcac1b0
Edward Potter <74555065+TA-Solaris@users.noreply.github.com>
2024-10-05 18:58:18 +0100

    Restructured react

---
+++ react/src/components/Gallery.jsx
@@ -0,0 +1,18 @@
+function Gallery() {
+
+    // TODO - Load images from backend
+    // TODO - Add image caching
+
+    const galleryStyle = {
+        padding: "10px",
+    };
+
+    // TODO - Add images
+    return (
+        <section style={galleryStyle}>
+            <span>test</span>
+        </section>
+    );
+}
+
+export default Gallery;
---
+++ react/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Gallery from '../components/Gallery';
+
+function Home() {
+  return (
+    <section>
+      <Gallery />
+    </section>
+  );
+}
+
+export default Home;
